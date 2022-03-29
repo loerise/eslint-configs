80639b77b1ba6bc3f244951a9edd78d095c74907
Lester Lau <loerise@gmail.com>
2022-03-29 21:39:25 +0800

    feat: add eslint-plugin-react-hooks

--- eslint-config/index.js
+++ eslint-config/index.js
@@ -5,15 +5,16 @@ module.exports = {
     ecmaFeatures: {
       jsx: true,
     },
-    sourceType: 'module',
+    sourceType: "module",
   },
-  plugins: ['import'],
+  plugins: ["import"],
   extends: [
     "eslint:recommended",
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:react/recommended',
-    'prettier',
+    "plugin:react-hooks/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:react/recommended",
+    "prettier",
   ],
   env: {
     browser: true,
@@ -21,35 +22,31 @@ module.exports = {
     es2021: true,
   },
   rules: {
-    'import/dynamic-import-chunkname': 2,
-    'import/first': 2,
-    'import/newline-after-import': 2,
-    'import/no-duplicates': 2,
-    'import/no-named-as-default': 0,
-    'import/no-unresolved': 2,
-    'import/order': [
-      'error',
+    "react-hooks/rules-of-hooks": 0,
+    "import/default": 0,
+    "import/dynamic-import-chunkname": 2,
+    "import/first": 2,
+    "import/newline-after-import": 2,
+    "import/no-duplicates": 2,
+    "import/no-named-as-default": 0,
+    "import/no-unresolved": 2,
+    "import/order": [
+      "error",
       {
-        groups: [
-          'builtin',
-          'external',
-          'index',
-          ['sibling', 'parent'],
-          'object',
-        ],
+        groups: ["builtin", "external", "index", ["sibling", "parent"], "object"],
         alphabetize: {
-          order: 'ignore',
+          order: "ignore",
         },
-        'newlines-between': 'always',
+        "newlines-between": "always",
       },
     ],
-    'react/display-name': 0,
-    'react/prop-types': 0,
-    'no-console': 1,
+    "react/display-name": 0,
+    "react/prop-types": 0,
+    "no-console": 1,
   },
   settings: {
     react: {
-      version: 'detect',
+      version: "detect",
     },
   },
-};
+}
